c21fbb3c946a032ca9a1438b696189437bef6bea
William Wang <wow7@cornell.edu>
2019-04-17 21:21:02 -0400

    set up firebase

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const admin = require('firebase-admin');
+const serviceAccount = require('./service-account.json');
 
 const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: "https://kanban-9c05f.firebaseio.com"
+});
+
+const db = admin.firestore();
 
 // 404, no matching route found
 app.use((_, res) => {
