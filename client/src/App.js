57b874f45c8543f714623181c54a35abb5da823a
William Wang <wow7@cornell.edu>
2019-04-23 02:42:23 -0400

    added basic routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,40 @@
 import React, { Component } from 'react';
-import './App.css';
+import {
+    BrowserRouter as Router, Route, Link, Switch, Redirect } from "react-router-dom";
 import BoardList from './components/BoardList';
 import Login from './components/Login';
 import Signup from './components/Signup';
+import BadPage from './components/BadPage';
+import './App.css';
 
 class App extends Component {
     render() {
         return (
-            <div class="Container">
-                <BoardList />
-            </div>
+            <Router>
+                <div>
+                    <nav>
+                        <ul>
+                            <li>
+                                <Link to="/">Home</Link>
+                            </li>
+                            <li>
+                                <Link to="/about/">About</Link>
+                            </li>
+                            <li>
+                                <Link to="/users/">Users</Link>
+                            </li>
+                        </ul>
+                    </nav>
+                    <div className="Container">
+                        <Switch>
+                            <Route path="/" exact component={BoardList} />
+                            <Route path="/login/" component={Login} />
+                            <Route path="/signup/" component={Signup} />
+                            <Route component={BadPage} />
+                        </Switch>
+                    </div>
+                </div>
+            </Router>
         );
     }
 }
---
+++ client/src/components/BadPage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const BadPage = () => {
+    return (
+        <div className="Container">
+            <h1>404 No Page Found</h1>
+        </div>
+    );
+}
+
+export default BadPage;
