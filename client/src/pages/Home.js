9a48666cca00d03fbed7ccd0c3659a7d05d36d48
William Wang <wow7@cornell.edu>
2019-04-24 03:02:48 -0400

    implement logout button

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { withRouter } from "react-router-dom";
 import BoardList from '../components/BoardList';
 import './Home.css';
 
@@ -6,19 +7,38 @@ class Home extends Component {
     constructor(props) {
         super(props);
         this.state = {
+            user: ''
         };
+
+        this.logout = this.logout.bind(this);
+    }
+
+    logout = async (_) => {
+        const { setNotification } = this.props;
+        const response = await fetch('/api/logout');
+        const responseText = await response.json();
+        console.log(responseText);
+
+        if (responseText === 'success') {
+            this.props.history.push("/login");
+        }
+        else {
+            setNotification(responseText);
+        }
     }
 
     render() {
+        const { notification } = this.props;
         return (
             <div>
                 <h1>Welcome user!</h1>
                 <h1>Your Kanban Boards</h1>
-                <button type="button">Log Out</button>
+                <button type="button" onClick={this.logout}>Log Out</button>
+                { notification && <p>{notification}</p> }
                 <BoardList />
             </div>
         );
     }
 }
 
-export default Home;
+export default withRouter(Home);
