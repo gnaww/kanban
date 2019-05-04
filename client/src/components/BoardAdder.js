002686f1d4aa6ea002f510b5484857a8406a57ba
William Wang <wow7@cornell.edu>
2019-05-04 01:31:43 -0400

    implemented adding new boards client side

--- client/src/components/BoardAdder.js
+++ client/src/components/BoardAdder.js
@@ -1,11 +1,42 @@
-import React from 'react';
+import React, { Component } from 'react';
+import IconButton from '@material-ui/core/IconButton';
+import TextField from '@material-ui/core/TextField';
+import Add from '@material-ui/icons/Add';
 import styles from './BoardAdder.module.css';
 
-const BoardAdder = props => {
-    return (
-        <div>
-        </div>
-    );
+class BoardAdder extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            newBoardName: ''
+        };
+    }
+
+    handleBoardNameChange = (event) => {
+        this.setState({ newBoardName: event.target.value });
+    }
+
+    addBoard = () => {
+        const { handleAddBoard } = this.props;
+        handleAddBoard(this.state.newBoardName);
+    }
+
+    render() {
+        return (
+            <div className={styles.BoardAdder}>
+                <h2>Add a new board</h2>
+                <IconButton onClick={this.addBoard}>
+                    <Add />
+                </IconButton>
+                <TextField
+                    label="New board name"
+                    value={this.state.newBoardname}
+                    onChange={this.handleBoardNameChange}
+                    margin="normal"
+                />
+            </div>
+        );
+    }
 }
         
 export default BoardAdder;
--- client/src/components/BoardList.js
+++ client/src/components/BoardList.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import styles from './BoardList.module.css';
 import Board from './Board';
+import BoardAdder from './BoardAdder';
 
 const BoardList = props => {
-    const { boards } = props;
+    const { boards, handleAddBoard } = props;
     return (
         <div className={styles.BoardList}>
+            <BoardAdder handleAddBoard={handleAddBoard} />
             { boards.map((board, idx) => <Board key={idx} id={idx} name={board.name} items={board.items} />) }
         </div>
     );
